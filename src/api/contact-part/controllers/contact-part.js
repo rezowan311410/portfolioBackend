10abb3e7f5026e709b01347ff1efe286717f5f39
rezowan <rezowan311410@gmail.com>
2024-02-23 12:33:05 +0600

    code upload

---
+++ src/api/contact-part/controllers/contact-part.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-part controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::contact-part.contact-part');
---
+++ src/api/contact-part/services/contact-part.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-part service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::contact-part.contact-part');
