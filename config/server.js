995f62664d380215922c4943f94c3b748c140ea2
rezowan <rezowan311410@gmail.com>
2024-02-23 17:47:48 +0600

    jwt update

--- config/server.js
+++ config/server.js
@@ -2,9 +2,14 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS',['csm3lBOPZE5emvyTbE1VmfLGzTcyxQwP']),
+    keys: env.array('APP_KEYS', ['YfruBcnAyAIUIDgNYgVk4A==', 'QDT4Gl9T9pDC5GWKBgUYGQ==', 'ymCduXxkZqBW/9VGaj+j1A==', 'mdt+G//aeF/cHXZDY+A3zA==']),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  // Use environment variable for jwtSecret
+  'users-permissions': {
+    jwtSecret: env('JWT_SECRET', 'joaxIBD5qKwDEWTwuqLX/g=='),
+  },
 });
+
